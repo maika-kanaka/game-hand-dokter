769b79def943f5f57adcd141cb1d2e6d3c6e3fe9
Maika Kanaka <maika-kanaka@protonmail.com>
2024-10-29 14:47:13 +0700

    rubah jadi 60 detik dan tambah sensor kepala

--- src/app/page.js
+++ src/app/page.js
@@ -3,19 +3,27 @@
 import { useEffect, useState, useRef } from "react";
 import styles from "./page.module.css";
 import { Hands } from '@mediapipe/hands';
+import { FaceMesh } from '@mediapipe/face_mesh';
 import { Camera } from '@mediapipe/camera_utils';
 
 export default function Home() 
 {
   const [page, setPage] = useState('home');
   const [score, setScore] = useState(0);
+  const [numberOfIncrementScore, setNumberOfIncrementScore] = useState(1);
   const [lastScore, setLastScore] = useState(null);
   const [maxHealth, setMaxHealth] = useState(5);
-  const [countdown, setCountdown] = useState(120);
+  const [countdown, setCountdown] = useState(60);
   const [health, setHealth] = useState(maxHealth * 19);
   const [isGameFinish, setIsGameFinish] = useState(false);
 
+  const webcamBackgroundRef = useRef(null);
   const isGameFinishRef = useRef(isGameFinish);
+
+  const config = {
+    sensorHuman: 'head', // hand or head
+    sensorUsingJavascript: true
+  };
 
   useEffect(() => {
     isGameFinishRef.current = isGameFinish;
@@ -48,23 +56,26 @@ export default function Home()
     }
   }, [lastScore]);
 
-  const showScorePage = () => {
+  const showScorePage = () => 
+  {
       setLastScore(Math.floor(score / 19));
       setIsGameFinish(true); // Ensure game is marked as finished
   };
 
-  const changePage = (page) => {
+  const changePage = (page) => 
+  {
     setPage(page);
 
     if (page === 'game') {
       document.getElementById('backsound-game').play();
 
       // Ensure MediaPipe is fully loaded before starting the game
-      runMoveHand();
+      runMoveCollector();
     }
   };
 
-  const startCountdown = async () => {
+  const startCountdown = async () => 
+  {
     setInterval(() => {
       setCountdown((prevCountdown) => {
         if(prevCountdown <= 0){
@@ -78,7 +89,8 @@ export default function Home()
     }, 1000);
   };
 
-  const runItemFalls = async () => {
+  const runItemFalls = async () => 
+  {
     try {
       const response = await fetch('items/lists.json');
       const itemList = await response.json();
@@ -97,14 +109,14 @@ export default function Home()
         itemElement.style.height = 'auto';
         // itemElement.style.boxShadow = '0 0 100px 0 rgba(255, 255, 255, 0.5)';
         // itemElement.style.backgroundColor = 'rgba(255, 255, 255, 0.2)';
-        // itemElement.style.padding = '70px';
+        itemElement.style.padding = '70px';
         
         gameArea.appendChild(itemElement);
 
         const fall = () => {
           let topPosition = parseFloat(itemElement.style.top);
           if (topPosition < gameAreaRect.height) {
-            itemElement.style.top = `${topPosition + 17}px`;
+            itemElement.style.top = `${topPosition + 24}px`;
             requestAnimationFrame(fall);
           } else {
             gameArea.removeChild(itemElement);
@@ -122,7 +134,7 @@ export default function Home()
         createFallingItem(randomItem);
       };
 
-      const gameInterval = setInterval(spawnItems, 2000);
+      const gameInterval = setInterval(spawnItems, 2500);
 
       // Clean up function to stop the game
       return () => clearInterval(gameInterval);
@@ -131,7 +143,101 @@ export default function Home()
     }
   };
 
-  const runMoveHand = () => {
+  const runMoveCollector = () => 
+  {
+    if(config.sensorHuman === 'hand')
+    {
+      if(config.sensorUsingJavascript)
+      {
+        _runMoveHandJavascript();
+      }
+      else 
+      {
+        _runMoveHandPython();
+      }
+    }
+    else if(config.sensorHuman === 'head')
+    {
+      if(config.sensorUsingJavascript)
+      {
+        _runMoveHeadJavascript();
+        console.log('_runMoveHeadJavascript');
+      }
+      else 
+      {
+        _runMoveHeadPython();
+      }
+    }
+
+    // Load asset then runItemFalls
+    runItemFalls();
+
+    let collisionDetected = false;
+
+    const checkCollision = () => {
+      const handRect = handSensor.getBoundingClientRect();
+      const items = document.querySelectorAll('main img.item-falls');
+
+      items.forEach(item => {
+        const itemRect = item.getBoundingClientRect();
+        if (
+          handRect.left < itemRect.right &&
+          handRect.right > itemRect.left &&
+          handRect.top < itemRect.bottom &&
+          handRect.bottom > itemRect.top
+        ) {
+          if (!collisionDetected) {
+            collisionDetected = true;
+            setTimeout(() => collisionDetected = false, 300); // Debounce for 300ms
+
+            // Collision detected
+            item.style.transition = 'transform 0.3s ease-in-out';
+            item.style.transform = 'scale(1.6)';
+            setTimeout(() => {
+              item.remove();
+            }, 300);
+
+            // Add catch animation to handSensor
+            handSensor.style.transition = 'transform 0.2s ease-in-out';
+            handSensor.style.transform = 'scale(0.9)';
+            setTimeout(() => {
+              handSensor.style.transform = 'scale(1)';
+            }, 200);
+
+            // Play catch sound
+            document.getElementById('backsound-catch').play();
+
+            // Add score
+            if (!isGameFinishRef.current) {  // Check if the game is not finished
+              if (item.src.includes('/items/jahat.png')){
+                setScore((prevScore) => {
+                  console.log('Decrementing score by:', numberOfIncrementScore);
+                  return prevScore - numberOfIncrementScore;
+                });
+              } else {
+                setScore((prevScore) => {
+                  console.log('Incrementing score by:', numberOfIncrementScore);
+                  return prevScore + numberOfIncrementScore;
+                });
+              }
+            }
+          }
+        }
+      });
+    };
+
+    startCountdown();
+
+    const animationFrame = () => {
+      checkCollision();
+      requestAnimationFrame(animationFrame);
+    };
+
+    requestAnimationFrame(animationFrame);    
+  };
+
+  const _runMoveHandJavascript = () => 
+  {
     const handSensor = document.querySelector('#handSensor');
     const videoElement = document.createElement('video');
     videoElement.style.display = 'none';
@@ -169,65 +275,99 @@ export default function Home()
 
     camera.start();
 
-    // Load asset then runItemFalls
-    runItemFalls();
-
-    const checkCollision = () => {
-      const handRect = handSensor.getBoundingClientRect();
-      const items = document.querySelectorAll('main img.item-falls');
-
-      items.forEach(item => {
-        const itemRect = item.getBoundingClientRect();
-        if (
-          handRect.left < itemRect.right &&
-          handRect.right > itemRect.left &&
-          handRect.top < itemRect.bottom &&
-          handRect.bottom > itemRect.top
-        ) {
-          // Collision detected
-          item.style.transition = 'transform 0.3s ease-in-out';
-          item.style.transform = 'scale(1.6)';
-          setTimeout(() => {
-            item.remove();
-          }, 300);
-
-          // Add catch animation to handSensor
-          handSensor.style.transition = 'transform 0.2s ease-in-out';
-          handSensor.style.transform = 'scale(0.9)';
-          setTimeout(() => {
-            handSensor.style.transform = 'scale(1)';
-          }, 200);
-
-          // Play catch sound
-          document.getElementById('backsound-catch').play();
-
-          // Add score
-          if (!isGameFinishRef.current) {  // Check if the game is not finished
-            if (item.src.includes('/items/3.png') || item.src.includes('/items/4.png') || item.src.includes('/items/5.png') || item.src.includes('/items/6.png') || item.src.includes('/items/7.png') || item.src.includes('/items/8.png')) {
-              setScore((prevScore) => prevScore - 1);
-            } else {
-              setScore((prevScore) => prevScore + 1);
-            }
-          }
-        }
-      });
-    };
-
-    startCountdown();
-
-    const animationFrame = () => {
-      checkCollision();
-      requestAnimationFrame(animationFrame);
-    };
-
-    requestAnimationFrame(animationFrame);
-
-    // Clean up function
     return () => {
       camera.stop();
       document.body.removeChild(videoElement);
     };
   };
+
+  const _runMoveHandPython = () => 
+  {
+    const handleMouseMove = (event) => {
+      const gameArea = document.querySelector('main');
+      const gameAreaRect = gameArea.getBoundingClientRect();
+      const mouseX = event.clientX - gameAreaRect.left;
+      const handPosition = (mouseX / gameAreaRect.width) * 100;
+      handSensor.style.left = `${Math.max(0, Math.min(100, handPosition))}%`;
+    }
+
+    document.addEventListener('mousemove', handleMouseMove);
+
+    return () => {
+      document.removeEventListener('mousemove', handleMouseMove);
+    };
+  };
+
+  const _runMoveHeadJavascript = () => {
+    const videoElement = webcamBackgroundRef.current;
+    const faceMesh = new FaceMesh({
+      locateFile: (file) => `mediapipe/${file}`,
+      maxNumFaces: 1,
+      minDetectionConfidence: 0.5,
+      minTrackingConfidence: 0.5,
+    });
+
+    faceMesh.onResults((results) => {
+      if (results.multiFaceLandmarks && results.multiFaceLandmarks.length > 0) {
+        const faceLandmarks = results.multiFaceLandmarks[0];
+        const noseTip = faceLandmarks[1]; // Nose tip landmark
+        const gameArea = document.querySelector('main');
+        const gameAreaRect = gameArea.getBoundingClientRect();
+        const headPositionX = (noseTip.x * gameAreaRect.width) / gameAreaRect.width * 100;
+        const headPositionY = (noseTip.y * gameAreaRect.height) / gameAreaRect.height * 100;
+        
+        handSensor.style.left = `${Math.max(0, Math.min(100, headPositionX))}%`;
+      }
+    });
+
+    const camera = new Camera(videoElement, {
+      onFrame: async () => {
+        await faceMesh.send({ image: videoElement });
+      },
+      width: 640,
+      height: 480,
+    });
+
+    camera.start();
+
+    // const checkCollision = (headPositionX, headPositionY) => {
+    //   const gameArea = document.querySelector('main');
+    //   const gameAreaRect = gameArea.getBoundingClientRect();
+    //   const headSensor = document.getElementById('handSensor');
+    //   const headSensorRect = headSensor.getBoundingClientRect();
+
+    //   const headSensorX = (headSensorRect.left - gameAreaRect.left) / gameAreaRect.width * 100;
+    //   const headSensorY = (headSensorRect.top - gameAreaRect.top) / gameAreaRect.height * 100;
+
+    //   // Check if the head position collides with the head sensor
+    //   if (Math.abs(headPositionX - headSensorX) < 5 && Math.abs(headPositionY - headSensorY) < 5) {
+    //     // Collision detected, perform necessary actions
+    //     console.log('Collision detected with head sensor');
+    //   }
+    // };
+
+    return () => {
+      camera.stop();
+      document.body.removeChild(videoElement);
+    };
+  }
+
+  const _runMoveHeadPython = () => {
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices.getUserMedia({ video: true })
+        .then(function(stream) {
+          const webcamBackground = document.getElementById('webcamBackground');
+          if (webcamBackground) {
+            webcamBackground.srcObject = stream;
+          }
+        })
+        .catch(function(error) {
+          console.error("Error accessing webcam: ", error);
+        });
+    } else {
+      console.error("getUserMedia not supported on your browser!");
+    }
+  }
 
   return (
     <main className={styles.main}>
@@ -236,27 +376,40 @@ export default function Home()
       <audio id="backsound-loser" loop><source src="audios/game-over.mp3" type="audio/mp3" /></audio>
       <audio id="backsound-catch" loop><source src="audios/flip-correct.mp3" type="audio/mp3" /></audio>
       <img id="handSensor" src="imgs/hand-sensor.png" style={{display: page === 'game' ? 'block' : 'none'}} className={styles.handSensor} />
+      <video id="webcamBackground" ref={webcamBackgroundRef} autoPlay muted style={{
+          position: 'fixed',
+          top: 0,
+          left: 0,
+          width: '100vw',
+          height: '100vh',
+          objectFit: 'cover',
+          // opacity: 0.3,
+          opacity: 1,
+          zIndex: -1,
+          display: page === 'game' ? 'block' : 'none'
+        }}></video>
 
       {page === 'home' && (
-        <video src="videos/itemfall-homepage2.mp4" onClick={() => changePage('game')} className={styles.videoBackground} autoPlay muted loop />
+        <video src="videos/itemfall-homepage.mp4" onClick={() => changePage('game')} className={styles.videoBackground} autoPlay muted loop />
       )}
 
       {page === 'game' && (
         <>
-          <img src="imgs/kvgames-itemfalls.jpg" className={styles.videoBackground} />
           {/* <img src="/imgs/framegames4.png" className={styles.frameGame4} /> */}
           <div className={styles.score}>
             <div> 
-            Score: {score ? Math.floor(score / 4) : 0}
+            Score: {score}
             </div>
             <div className={styles.countdown}>
             {countdown}
             </div>
           </div>
           <div className={styles.healthWrapper}>
-            {Array.from({ length: health / 19 }, (_, index) => (
+            {/* {Array.from({ length: health / 19 }, (_, index) => (
               <img key={index} src="/imgs/heart.png" className={styles.health} />
-            ))}
+            ))} */}
+
+            <img src="/imgs/logo.png" className={styles.health} style={{width: '550px'}} />
           </div>
 
           <div id="gameArea" className={styles.gameArea}>
@@ -277,7 +430,7 @@ export default function Home()
               zIndex: 1000
             }}>
               <div className={styles.lastScore}>
-                YOUR SCORE: {Math.floor(score / 4)}
+                YOUR SCORE: {Math.floor(score)}
               </div>
               <video
                 src="videos/itemfall-gameover.mp4"
